675a31c9949ba1a241eb43ef61921a728a0a66d5
bhanuprakash9980 <bhanuprakashshettigar@gmail.com>
2020-08-18 12:20:29 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import torch
 import transformers
 app = Flask(__name__)
@@ -6,7 +6,7 @@ gpt_tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
 gpt_model = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
 
 
-def gen_text(prompt_text, tokenizer=gpt_tokenizer, model=gpt_model, n_seqs=1, max_length=1000):
+def gen_text(prompt_text, tokenizer=gpt_tokenizer, model=gpt_model, n_seqs=1, max_length=10000):
     encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
     output_sequences = model.generate(input_ids=encoded_prompt, max_length=max_length+len(encoded_prompt), temperature=1.0, top_k=40, top_p=0.9, repetition_penalty=1.2, do_sample=True, num_return_sequences=n_seqs)
     if len(output_sequences.shape) > 2:
@@ -20,19 +20,18 @@ def gen_text(prompt_text, tokenizer=gpt_tokenizer, model=gpt_model, n_seqs=1, ma
     return generated_sequences[0]
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
 def home():
-    if request.method == "POST":
-        result = gen_text(request.form["text"])
-        text = result.rsplit(' ', 1)[0]
-        return render_template('home.html', data=text)
-    else:
-        return render_template('home.html')
+    return render_template('home.html')
 
 
-@app.route('/hello')
-def hello():
-    return "hello world"
+@app.route('/gen',methods=["POST"])
+def gen():
+    text=request.form["text"]
+    result = gen_text(text,max_length=(len(text)+1000))
+    result=result.replace('<|endoftext|>','')
+    return jsonify({'text' : result})
+
 
 
 if __name__ == "__main__":
---
+++ static/effects.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	$('textarea').on('input', function () { 
+		this.style.height = 'auto'; 
+		  
+		this.style.height =  
+				(this.scrollHeight) + 'px'; 
+	}); 
+
+	$('#gen').on('click', function(event) {
+        
+		$.ajax({
+			data : {
+				text:$('#textarea1').val()
+			},
+			type : 'POST',
+			url : '/gen'
+		})
+		.done(function(data) {
+          
+            if(data.text)
+            {
+                $("#textoutput").text(data.text);
+                $("#more").show()
+            }
+
+		});
+
+		event.preventDefault();
+
+    });
+    $('#more').on('click', function(event) {
+        
+		$.ajax({
+			data : {
+				text:$('#textoutput').text()
+			},
+			type : 'POST',
+			url : '/gen'
+		})
+		.done(function(data) {
+       
+            if(data.text)
+            {   data.text=data.text.replace('<|endoftext|>','');
+                $("#textoutput").text(data.text);
+                $("#more").show()
+            }
+
+		});
+
+		event.preventDefault();
+
+	});
+
+});
